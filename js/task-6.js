9355d9e11261128bce0dce09d1927c2e25bc4ea1
Andrej Sokolov <99323383+CyberPanda263@users.noreply.github.com>
2024-04-30 18:38:51 +0300

    Fixes after testing

--- js/task-6.js
+++ js/task-6.js
@@ -1,48 +1,42 @@
+const createButton = document.querySelector("[data-create]");
+const destroyButton = document.querySelector("[data-destroy]")
+const boxes = document.querySelector("#boxes");
+let amount;
+
+createButton.addEventListener("click", create);
+destroyButton.addEventListener("click", deleteEl);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
 
-const createButton = document.querySelector("[data-create]");
-const destroyButton = document.querySelector("[data-destroy]")
-let amount;
-
 function create() {
   const input = document.querySelector("input");
-  const val = input.target;
   const value = Number(input.value);
   if(value >= 1 && value <= 100) {
     amount = value;
-    const div = document.querySelector("#boxes");
-    if(div.children.length > 0) {
-      deleteEl()
-    }
+    deleteEl();
     createBoxes(amount);
   }
   document.querySelector('input[type="number"]').value = "";
 }
 
 function createBoxes(amount) {
-  const boxes = document.querySelector("#boxes");
-  let sise = 30;
+  let size = 30;
   const boxArray = [];
   for (let i = 0; i < amount; i++) {
   const div = document.createElement("div");
-  div.style.width = `${sise}px`;
-  div.style.height = `${sise}px`;
+  div.style.width = `${size}px`;
+  div.style.height = `${size}px`;
   div.style.backgroundColor = getRandomHexColor();
   boxArray.push(div);
-  sise = sise + 10;
+  size = size + 10;
   }
   boxes.append(...boxArray);
 }
 
 function deleteEl() {
-  const del = document.querySelector("#boxes");
-  del.innerHTML = "";
+  boxes.innerHTML = "";
 }
-
-
-createButton.onclick = create;
-destroyButton.onclick = deleteEl;
